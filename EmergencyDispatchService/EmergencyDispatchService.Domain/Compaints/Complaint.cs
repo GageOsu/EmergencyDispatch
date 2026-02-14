229f0f2a82ffe038c94748e8a8881004e1ec8c09
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-14 17:14:14 +0300

    Create base Complaint

---
+++ EmergencyDispatchService/EmergencyDispatchService.Domain/Compaints/Complaint.cs
@@ -0,0 +1,24 @@
+﻿using EmergencyDispatchService.Domain.Common;
+
+namespace EmergencyDispatchService.Domain.Compaints;
+
+public class Complaint : Entity
+{
+    public Guid UserCreateId { get; } 
+
+    public Guid UserAcceptId { get; }
+
+    //TODO: Сделать enum статуса жалобы
+
+    public DateTime CreateDate { get; }
+
+    public DateTime CompletionDate { get; }
+
+
+    //TODO: Добавить возможность добавления Фотографии
+    public string Description { get; } = null!;
+
+
+
+
+}

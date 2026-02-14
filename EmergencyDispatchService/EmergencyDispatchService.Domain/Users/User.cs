ad8bfbffb9906f35030fb9631a3a595337c6537e
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-14 16:51:38 +0300

    Create base User

---
+++ EmergencyDispatchService/EmergencyDispatchService.Domain/Users/User.cs
@@ -0,0 +1,24 @@
+﻿using EmergencyDispatchService.Domain.Common;
+
+namespace EmergencyDispatchService.Domain.Users;
+
+public class User : Entity
+{
+    public string FirstName { get; } = null!;
+
+    public string LastName { get; } = null!;
+
+    public string PhoneNumber { get; } = null!;
+
+    public User(
+        string firstName,
+        string lastName,
+        string phoneNumber,
+        Guid? id = null)
+        : base(id ?? Guid.NewGuid())
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        PhoneNumber = phoneNumber;
+    }
+}

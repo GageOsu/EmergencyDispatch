b7b90e6e6d6c90d942f053566dc8b2d45859c7c1
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-16 17:50:09 +0300

    Create base DI Infrastructure

---
+++ EmergencyDispatchService/EmergencyDispatchService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿
+using EmergencyDispatchService.Application.Common.Interface;
+using EmergencyDispatchService.Infrastructure.Common;
+using EmergencyDispatchService.Infrastructure.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmergencyDispatchService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        var stringConnection = configuration.GetConnectionString("ConnectionStrings");
+        services.AddDbContext<EmergencyDispatchServiceDbContext>(options =>
+        options.UseNpgsql(stringConnection));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork>(serviceProvider => serviceProvider.GetRequiredService<IUnitOfWork>());
+
+        return services;
+    }
+}
--- EmergencyDispatchService/EmergencyDispatchService.Infrastructure/Users/UserRepository.cs
+++ EmergencyDispatchService/EmergencyDispatchService.Infrastructure/Users/UserRepository.cs
@@ -1,5 +1,25 @@
-﻿namespace EmergencyDispatchService.Infrastructure.Users;
+﻿using EmergencyDispatchService.Application.Common.Interface;
+using EmergencyDispatchService.Domain.Users;
+using EmergencyDispatchService.Infrastructure.Common;
 
-public class UserRepository
+namespace EmergencyDispatchService.Infrastructure.Users;
+
+public class UserRepository : IUserRepository
 {
+    private readonly EmergencyDispatchServiceDbContext _context;
+
+    public UserRepository(EmergencyDispatchServiceDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddUserAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public async Task<User?> GetByIdUserAsync(Guid userId)
+    {
+        return await _context.Users.FindAsync(userId);
+    }
 }

71842ef603916ec050c4a3f52ee2990ae6d9b05a
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-16 17:25:45 +0300

    create base UserConfiguration

---
+++ EmergencyDispatchService/EmergencyDispatchService.Infrastructure/Users/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EmergencyDispatchService.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EmergencyDispatchService.Infrastructure.Users;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder .HasKey(u => u.Id);
+
+        builder.Property(u => u.Id)
+            .ValueGeneratedNever();
+
+        builder.Property("FirstName")
+            .HasColumnName("FirstName");
+
+        builder.Property("LastName")
+            .HasColumnName("LastName");
+
+        builder.Property("PhoneNumber")
+            .HasColumnName("PhoneNumber");
+    }
+}

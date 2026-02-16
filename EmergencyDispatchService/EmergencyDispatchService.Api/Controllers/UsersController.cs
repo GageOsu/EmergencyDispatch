599aac011d5201a9d55fcb9d67f7f46a09c03cab
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-16 17:59:00 +0300

    Create User endpoint createUser

---
+++ EmergencyDispatchService/EmergencyDispatchService.Api/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using EmergencyDispatchService.Contracts.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmergencyDispatchService.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateUserAsync(CreateUserRequest)
+}

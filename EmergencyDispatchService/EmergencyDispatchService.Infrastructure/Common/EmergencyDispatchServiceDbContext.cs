59a6c599050e22800692d3948a4737f88a450d87
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-16 17:21:06 +0300

    create DbContext, base Users

--- EmergencyDispatchService/EmergencyDispatchService.Infrastructure/Common/EmergencyDispatchServiceDbContext.cs
+++ EmergencyDispatchService/EmergencyDispatchService.Infrastructure/Common/EmergencyDispatchServiceDbContext.cs
@@ -1,5 +1,29 @@
-﻿namespace EmergencyDispatchService.Infrastructure.Common;
+﻿using EmergencyDispatchService.Application.Common.Interface;
+using EmergencyDispatchService.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
-public class EmergencyDispatchServiceDbContext
+namespace EmergencyDispatchService.Infrastructure.Common;
+
+public class EmergencyDispatchServiceDbContext : DbContext, IUnitOfWork
 {
+
+    public DbSet<User> Users { get; set; } = null!;
+
+    public EmergencyDispatchServiceDbContext(DbContextOptions options)
+        : base(options)
+    {
+
+    }
+
+    public async Task CommitChangesAsync()
+    {
+        await base.SaveChangesAsync();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
 }

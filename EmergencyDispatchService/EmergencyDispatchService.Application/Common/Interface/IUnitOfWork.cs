982358acbb05d342e28cae973dab0e069bf51aed
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-16 16:58:34 +0300

    Create base User interface Repository

---
+++ EmergencyDispatchService/EmergencyDispatchService.Application/Common/Interface/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace EmergencyDispatchService.Application.Common.Interface;
+
+public interface IUnitOfWork
+{
+    Task CommitChangesAsync();
+}
---
+++ EmergencyDispatchService/EmergencyDispatchService.Application/Common/Interface/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using EmergencyDispatchService.Domain.Users;
+
+namespace EmergencyDispatchService.Application.Common.Interface;
+
+public interface IUserRepository
+{
+    Task AddUserAsync(User user);
+
+    Task<User?> GetByIdUserAsync(Guid userId);
+}

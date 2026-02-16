fecd1efd7c6e5cd582859df96a5fd4b2abb8491e
Gage_Osu <87873807+GageOsu@users.noreply.github.com>
2026-02-16 17:10:54 +0300

    Create base User GetByIdQuery

---
+++ EmergencyDispatchService/EmergencyDispatchService.Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,23 @@
+﻿using EmergencyDispatchService.Application.Common.Interface;
+using EmergencyDispatchService.Domain.Users;
+
+namespace EmergencyDispatchService.Application.Users.Queries.GetUser;
+
+public class GetUserQuery
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetUserQuery(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<User?> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdUserAsync(id);
+
+        return user is null 
+            ? null 
+            : user;
+    }
+}
